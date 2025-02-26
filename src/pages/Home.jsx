776ead3f2f8832f97a2b7eef0aca81969aa2b7cd
Pranjal <pandeypranjal264@gmail.com>
2025-02-26 17:59:32 +0530

    updated_code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import JobListing from '../components/JobListing'
+import AppDowload from '../components/AppDowload'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Navbar/>
     <Hero/>
     <JobListing/>
+    <AppDowload/>
+    <Footer/>
     </div>
   )
 }
